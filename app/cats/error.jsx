52e03ec774a16b13551e65043061566d2146b5d3
salysaly <anyawee.sr@gmail.com>
2024-07-08 23:14:38 +0700

    chore(page): Error, NotFound

---
+++ app/cats/error.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Error = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>An error occurred!</h2>
+      <span>Oops! We could not fetch the purrfect profiles</span>
+    </div>
+  );
+};
+
+export default Error;
---
+++ app/not-found.jsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Uh-oh! We could not find that page</h2>
+      <span>
+        Looks like our furballs are hiding! Please check the URL or go back to
+        the homepage
+      </span>
+    </div>
+  );
+};
+
+export default NotFound;
