e5214fc208ae7bf00ac33a5b9f8ab4d34b576164
salysaly <anyawee.sr@gmail.com>
2024-07-09 23:07:55 +0700

    chore(page): cats/share

--- app/cats/share/page.jsx
+++ app/cats/share/page.jsx
@@ -1,7 +1,63 @@
 const share = () => {
   return (
     <>
-      <h3>share</h3>
+      <header style={{ marginBottom: 50 }}>
+        <h3>Add Your Cat's Profile</h3>
+        <h5>help them find the purrfect match!</h5>
+      </header>
+      <main>
+        <form>
+          <div
+            style={{
+              display: "flex",
+              gap: 20,
+            }}
+          >
+            <div>
+              <label htmlFor="cat-name">Cat's Name:</label>
+              <input id="cat-name" name="cat-name" type="text" required />
+            </div>
+            <div
+              style={{
+                display: "flex",
+              }}
+            >
+              <label htmlFor="cat-gender">Gender:</label>
+              <input id="cat-gender" name="cat-gender" type="text" required />
+            </div>
+            <div>
+              <label htmlFor="cat-age">Age:</label>
+              <input id="cat-age" name="cat-age" type="text" required />
+            </div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              gap: 20,
+            }}
+          >
+            <div>
+              <label htmlFor="owner-name">Your name:</label>
+              <input id="owner-name" name="owner-name" type="text" required />
+            </div>
+            <div>
+              <label htmlFor="owner-email">Your email:</label>
+              <input
+                id="owner-email"
+                name="owner-email"
+                type="email"
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="description">Description: </label>
+            <input id="description" name="description" type="text" required />
+          </div>
+          <button type="submit">submit</button>
+        </form>
+      </main>
     </>
   );
 };
